8eb2f053967953722641a6419c958f2c5594ceb7
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-06-03 12:32:47 +0600

    faculty created

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { userRoute } from "../modules/user/user.route";
 import { StudentRoute } from "../modules/student/student.route";
 import { semesterRouter } from "../modules/semester/semester.route";
+import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route";
 
 const router = Router();
 
@@ -17,6 +18,10 @@ const moduleRoutes = [
   {
     path: "/semesters",
     route: semesterRouter,
+  },
+  {
+    path: "/faculties",
+    route: AcademicFacultyRoutes,
   },
 ];
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
