9d77186faf67a351d14578e45a625eda1f61d85d
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-06-08 15:10:44 +0600

    faculty created

---
+++ src/app/modules/Faculty/faculty.route.ts
@@ -0,0 +1,20 @@
+import { updateFacultyValidationSchema } from "./faculty.validation";
+import express from "express";
+import validateRequest from "../../middlewares/validateRequest";
+import { FacultyControllers } from "./faculty.controller";
+
+const router = express.Router();
+
+router.get("/:id", FacultyControllers.getSingleFaculty);
+
+router.patch(
+  "/:id",
+  validateRequest(updateFacultyValidationSchema),
+  FacultyControllers.updateFaculty
+);
+
+router.delete("/:id", FacultyControllers.deleteFaculty);
+
+router.get("/", FacultyControllers.getAllFaculties);
+
+export const FacultyRoutes = router;
